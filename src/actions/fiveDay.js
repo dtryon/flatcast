dfa93c98dbe6804f3e71d8694d60d1dd2f90d9f7
Davin Tryon <davintryon@gmail.com>
2016-06-29 11:41:58 +0100

    add city input and styles

--- src/actions/fiveDay.js
+++ src/actions/fiveDay.js
@@ -36,3 +36,4 @@ export const selectDay = (selectedDay) => {
         selectedDay
     }
 }
+
--- src/components/FlatCast.js
+++ src/components/FlatCast.js
@@ -10,18 +10,28 @@ import HourBreakdown from './HourBreakdown';
 
 export class FlatCast extends React.Component {
     componentDidMount() {
-        this.props.init('london,uk');
+        this.props.init('london,gb');
     }
 
     render() {
         const { days, selectedDay, city } = this.props;
-        return <div className="FlatCast">
-            <h2>{city}</h2>
+        return <section className="FlatCast">
+            <div className="grid">
+                <div className="grid-row">
+                    <div className="FlatCast_city grid-col-xs-12 grid-col-md-6">
+                        <h2 className="FlatCast_city_value">{city}</h2>
+                    </div>
+                    <div className="grid-col-xs-12 grid-col-md-6">
+                        <input className="FlatCast_city_input" placeholder="city - countryCode" type="text" defaultValue={city} ref="cityInput"/>
+                        <button className="FlatCast_city_submit" onClick={() => this.props.changeCity(this.refs.cityInput.value)}>Get Flatcast</button>
+                    </div>
+                </div>
+            </div>
             <div className="FlatCast_5day grid">
                 <DayBreakdown {...this.props} />
                 { days && days[selectedDay] && <HourBreakdown hours={days[selectedDay].hours}/> }
             </div>
-        </div>;
+        </section>;
     }
 }
 
@@ -33,6 +43,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     init: (city) => dispatch(getForecastForCity(city)),
-    selectDay: (day) => dispatch(selectDay(day))
+    selectDay: (day) => dispatch(selectDay(day)),
+    changeCity: (city) => dispatch(getForecastForCity(city))
 });
 export default connect(mapStateToProps, mapDispatchToProps)(FlatCast);
